777f25fcc11dd45ed7c8597a76a126bd62f0fc76
RT44n <147970845+RT44n@users.noreply.github.com.>
2024-04-13 10:50:02 +0530

    pass first test

--- tests/ShoppingPage.test.jsx
+++ tests/ShoppingPage.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import ShoppingPage from "../components/ShoppingPage";
+import ShoppingPage from "../src/components/ShoppingPage";
 
 describe("App component", () => {
   it("renders correct heading", () => {
