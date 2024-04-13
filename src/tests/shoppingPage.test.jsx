b7994ff3304022127da97713ec12b43b8092216f
RT44n <147970845+RT44n@users.noreply.github.com.>
2024-04-13 10:44:26 +0530

    first test

---
+++ src/tests/shoppingPage.test.jsx
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import ShoppingPage from "../components/ShoppingPage";
+
+describe("App component", () => {
+  it("renders correct heading", () => {
+    render(<ShoppingPage />);
+    expect(screen.getByRole("heading").textContent).toMatch(/Shopping Page/i);
+  });
+});
